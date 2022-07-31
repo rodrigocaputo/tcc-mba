a21741901d7755fa7e40a203077f143fd5264a1f
rodrigocaputo <rodrigokaputo@gmail.com>
2022-07-31 19:37:06 -0300

    Ativação do Bluetooth e consulta de dispositivos pareados

--- app/src/main/java/com/example/tccmba/MainActivity.java
+++ app/src/main/java/com/example/tccmba/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.tccmba;
 
+import static android.support.v4.app.ActivityCompat.startActivityForResult;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.view.Menu;
 import android.support.design.widget.Snackbar;
@@ -13,10 +21,18 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.widget.Toast;
 
 import com.example.tccmba.databinding.ActivityMainBinding;
 
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static int ENABLE_BLUETOOTH = 1;
+    public static int SELECT_PAIRED_DEVICE = 2;
+    public static int SELECT_DISCOVERED_DEVICE = 3;
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
@@ -47,6 +63,51 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(), "Que pena! Hardware Bluetooth não está funcionando :(", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Ótimo! O Bluetooth está funcionando :)", Toast.LENGTH_SHORT).show();
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    Toast.makeText(getApplicationContext(), "Solicitando ativação do Bluetooth...", Snackbar.LENGTH_SHORT).show();
+                }
+                startActivityForResult(enableBtIntent, ENABLE_BLUETOOTH);//, REQUEST_ENABLE_BT);
+            } else {
+                Toast.makeText(getApplicationContext(), "Bluetooth já ativado :)", Snackbar.LENGTH_SHORT).show();
+                Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+                if (pairedDevices.size() > 0) {
+                    // There are paired devices. Get the name and address of each paired device.
+                    for (BluetoothDevice device : pairedDevices) {
+                        String deviceName = device.getName();
+                        String deviceHardwareAddress = device.getAddress(); // MAC address
+//                        Toast.makeText(getApplicationContext(), deviceName + ": " + deviceHardwareAddress, Snackbar.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == ENABLE_BLUETOOTH) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(getApplicationContext(), "Bluetooth ativado :D", Snackbar.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Bluetooth não ativado :(", Snackbar.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
